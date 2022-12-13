c7d39cea1ad1f494d2d2e67aeb9fe1193dab93b0
Vothuong <nguyenkieuvuong@gmail.com>
2022-12-13 15:40:10 +0700

    state

--- src/views/example/TestComponent.js
+++ src/views/example/TestComponent.js
@@ -1,7 +1,20 @@
 import React from "react";
+
 class TestComponent extends React.Component {
+  state = {
+    name: "Lân",
+    Class: "IT",
+  };
   render() {
-    return <div>Hello my component</div>;
+    return (
+      <>
+        <div>
+          <input type="text" value={this.state.name} />
+        </div>
+        <div>Tôi học Reactjs.Tên là {this.state["name"]}</div>
+        <div>Phòng ban: {this.state.Class}</div>
+      </>
+    );
   }
 }
 export default TestComponent;
