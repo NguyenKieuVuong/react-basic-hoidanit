1dbe8d845d2b870804abd633c04d83326472476f
Vothuong <nguyenkieuvuong@gmail.com>
2022-12-13 15:58:46 +0700

    State - DOM event - change State & re-render

--- src/views/example/TestComponent.js
+++ src/views/example/TestComponent.js
@@ -5,11 +5,21 @@ class TestComponent extends React.Component {
     name: "Lân",
     Class: "IT",
   };
+  onHandleChangeValue = (event) => {
+    console.log(">> event target value", event.target.value);
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
     return (
       <>
         <div>
-          <input type="text" value={this.state.name} />
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.onHandleChangeValue(event)}
+          />
         </div>
         <div>Tôi học Reactjs.Tên là {this.state["name"]}</div>
         <div>Phòng ban: {this.state.Class}</div>
